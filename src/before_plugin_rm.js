6fed78ac83542c9c682a2330a708353273503ab1
João Grazina <jgrazina@gmail.com>
2018-05-03 15:33:58 +0100

    Update before_plugin_rm.js

--- src/before_plugin_rm.js
+++ src/before_plugin_rm.js
@@ -13,8 +13,9 @@ module.exports = function(ctx) {
     NODE_MODULES_DIR = path.join(projectRoot, "node_modules");
   }
 
-  var xml2js = require(path.join(NODE_MODULES_DIR, 'xml2js'));
-
+  var minimist = requireCordovaModule('minimist');
+  var xml2js = requireCordovaModule('xml2js');
+  
   return Q.Promise(function(resolve, reject, notify) {
     //---------------------------
     // Read the config.xml file
